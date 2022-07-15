d0e675cd3652308f9fc4b5783fd9168a7870a368
CanutoIX <caanutto@gmail.com>
2022-07-15 20:09:52 -0300

    Exercicio 3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/Exercicio 3.js
@@ -0,0 +1,8 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+}
+let avg = sum/numbers.length;
+console.log(avg);
